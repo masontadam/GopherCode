4c2a3b15fa5e20dfbf48a5cedd13a874f18234bc
Mason Adam <masontadam@gmail.com>
2020-01-18 21:30:50 -0500

    create base home and editor web pages, simplify server code

--- src/main/main.go
+++ src/main/main.go
@@ -6,85 +6,43 @@ package main
 
 import (
 	"html/template"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
-	"regexp"
+	// "io/ioutil"
+	// "regexp"
 )
 
+// data to pass to templates
+// TODO: port this to JSON/settings file
 type Page struct {
 	Title string
-	Body  []byte
 }
 
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+// Future home of a web based text editor
+func editorHandler(w http.ResponseWriter, req *http.Request) {
+	data := Page{"GopherCode"}
+	renderTemplate(w, "editor", &data)
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
+// Handler for Landing/Home Page
+func landingHandler(w http.ResponseWriter, req *http.Request) {
+	data := Page{"GopherCode"}
+	renderTemplate(w, "home", &data)
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err := p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
-var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+// Gather Templates
+// TODO: progamatically gather templates
+var templates = template.Must(template.ParseFiles("tmpl/editor.html", "tmpl/home.html"))
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-
-func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.URL.Path)
-		if m == nil {
-			http.NotFound(w, r)
-			return
-		}
-		fn(w, r, m[2])
-	}
+	fmt.Println(tmpl)
+	templates.ExecuteTemplate(w, tmpl+".html", p)	
 }
 
 func main() {
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/editor/", editorHandler)
+	http.HandleFunc("/", landingHandler)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
